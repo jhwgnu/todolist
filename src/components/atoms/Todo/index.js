f0f61f3edbcd935b9162c963f41eea0dbd0a46ed
jhwgnu <jhwon11@snu.ac.kr>
2017-04-28 21:20:22 +0900

    Todo atom 구현

--- src/components/atoms/Todo/index.js
+++ src/components/atoms/Todo/index.js
@@ -2,18 +2,26 @@ import { PropTypes } from 'react'
 import styled from 'styled-components'
 import { font, palette } from 'styled-theme'
 
-const Todo = styled.span`
-  font-family: ${font('primary')};
-  color: ${palette({ grayscale: 0 }, 1)};
+const Styledli = styled.li `
+    font-family: ${font('primary')};
 `
 
-Todo.propTypes = {
-  palette: PropTypes.string,
-  reverse: PropTypes.bool,
-}
+const Todo = ({ onClick, completed, text }) => (
+    <Styledli
+        onClick={onClick}
+        style={{
+            textDecoration: completed ? 'line-through' : 'none'
+        }}
+    >
+    {text}
+    </Styledli>
+)
 
-Todo.defaultProps = {
-  palette: 'grayscale',
+Todo.PropTypes = {
+  onClick: PropTypes.func.isRequired,
+  completed: PropTypes.bool.isRequired,
+  text: PropTypes.string.isRequired,
+  reverse: PropTypes.bool
 }
 
 export default Todo
