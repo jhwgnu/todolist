823db6580aa7f277d8bd8a4291a90505d1c2b495
jhwgnu <jhwon11@snu.ac.kr>
2017-04-28 21:32:51 +0900

    AddTodo molecule 구현

--- src/components/molecules/AddTodo/index.js
+++ src/components/molecules/AddTodo/index.js
@@ -1,19 +1,40 @@
 import React, { PropTypes } from 'react'
 import styled from 'styled-components'
 import { font, palette } from 'styled-theme'
+import Button from '../../atoms/Button'
 
 const Wrapper = styled.div`
   font-family: ${font('primary')};
   color: ${palette('grayscale', 0)};
 `
 
-const AddTodo = ({ children, ...props }) => {
-  return (
-    <Wrapper {...props}>
-      {children}
-    </Wrapper>
-  )
-}
+export const AddTodo = ({ statefunction, onAddTodo, onPostTodo }) => {
+  let input;
+
+  // add 버튼
+  const onSubmit = () => {
+    if (input != undefined) {
+      onAddTodo(input.value);
+      input.value = '';
+    }
+  };
+
+  // post 버튼
+  const onPut = () => {
+    if (input != undefined) {
+      onPostTodo(input.value);
+      input.value = '';
+    }
+  };
+
+  return {
+    <div>
+      <input ref={node => {input = node;}} />
+      <Button type="submit" onClick={onSubmit}>ADD Todo</Button?
+      <Button type="submit" onClick={onPut}> POST todo</Button>
+    </div>
+  };
+};
 
 AddTodo.propTypes = {
   reverse: PropTypes.bool,
